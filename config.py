16d1635c460e0cddc79e085aca33ff519f8fc444
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-08-28 03:36:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# Channel and Group Links
+CHANNEL_LINK = "https://t.me/Love_Angels_10"
+OWNER_CONTACT = "https://t.me/Love_Angels_10"
+GROUP_LINK = "https://t.me/Love_Angels_10"
