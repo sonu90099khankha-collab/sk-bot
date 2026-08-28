b6d86b4e6e253e0b581e8324597805e2173e2009
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-08-28 12:31:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-import os
 import asyncio
-from pyrogram import Client, filters
-from pytgcalls import PyTgCalls
-from pytgcalls.types import AudioStream, VideoStream
-from flask import Flask
-from threading import Thread
-
-# Event loop fix
 try:
     asyncio.get_event_loop()
 except RuntimeError:
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+
+from pyrogram import Client, filters
+import random
+from flask import Flask
+from threading import Thread
+import os
+from pytgcalls import PyTgCalls
+from pytgcalls.types import AudioStream, VideoStream
 
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
@@ -30,11 +30,9 @@ app = Client(
 
 call_py = PyTgCalls(app)
 
-# Aliases for bot.py compatibility
 AudioPiped = AudioStream
 VideoPiped = VideoStream
 
-# Flask web server to keep Render alive
 flask_app = Flask(__name__)
 
 @flask_app.route('/')
@@ -45,7 +43,6 @@ def run_web():
     port = int(os.environ.get("PORT", 8080))
     flask_app.run(host="0.0.0.0", port=port)
 
-# Run Flask in background thread
 t = Thread(target=run_web)
 t.daemon = True
 t.start()
