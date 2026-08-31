717886edff974e5642ea1c85edab9c89ae664f1a
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-08-31 15:08:53 +0530

    Update stream_helper.py

--- stream_helper.py
+++ stream_helper.py
@@ -1,21 +1,9 @@
-from pytgcalls import PyTgCalls
-from pytgcalls.types import StreamAudio
-
 def setup_calls(app):
-    return PyTgCalls(app)
+    return app
 
 async def play_audio_stream(call_py, chat_id, url):
-    try:
-        await call_py.join_group_call(
-            chat_id,
-            StreamAudio(url)
-        )
-    except Exception:
-        pass
+    pass
 
 async def stop_stream(call_py, chat_id):
-    try:
-        await call_py.leave_group_call(chat_id)
-    except Exception:
-        pass
-        
+    pass
+    
