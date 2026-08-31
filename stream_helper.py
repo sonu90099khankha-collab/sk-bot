3e4631366f5bea6714b50e7f55688f2d84a28d8c
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-08-31 14:26:30 +0530

    Update stream_helper.py

--- stream_helper.py
+++ stream_helper.py
@@ -1,18 +1,25 @@
 from pytgcalls import PyTgCalls
 from pytgcalls.types import MediaStream
+from pytgcalls.exceptions import GroupCallNotFound
 
 def setup_calls(app):
     return PyTgCalls(app)
 
 async def play_audio_stream(call_py, chat_id, url):
-    await call_py.join_group_call(
-        chat_id,
-        MediaStream(
-            url,
-            stream_type=MediaStream.Audio
+    try:
+        await call_py.join_group_call(
+            chat_id,
+            MediaStream(
+                url,
+                stream_type=MediaStream.Audio
+            )
         )
-    )
+    except GroupCallNotFound:
+        pass
 
 async def stop_stream(call_py, chat_id):
-    await call_py.leave_group_call(chat_id)
-    
+    try:
+        await call_py.leave_group_call(chat_id)
+    except Exception:
+        pass
+        
