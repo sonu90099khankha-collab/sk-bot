9a3a62f717f6ae8d74a4b6dc748ebd1f711d85ca
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-08-31 17:04:35 +0530

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-# Automatically install required packages if missing
-subprocess.run(["pip", "install", "-r", "requirements.txt"])
-
-# Run your main bot file
-os.system("python3 bot.py")
