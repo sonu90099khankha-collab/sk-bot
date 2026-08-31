f16b7499532101445547a7b6836996867bd456c9
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-08-31 13:22:27 +0530

    Update stream_helper.py

--- stream_helper.py
+++ stream_helper.py
@@ -1,5 +1,5 @@
 from pytgcalls import PyTgCalls
-from pytgcalls.types import MediaStream
+from pytgcalls.types import AudioPiped, VideoPiped
 
 def setup_calls(app):
     return PyTgCalls(app)
@@ -7,21 +7,15 @@ def setup_calls(app):
 async def play_audio_stream(call_py, chat_id, url):
     await call_py.join_group_call(
         chat_id,
-        MediaStream(
-            url,
-            stream_type=MediaStream.Audio
-        )
+        AudioPiped(url)
     )
 
 async def play_video_stream(call_py, chat_id, url):
     await call_py.join_group_call(
         chat_id,
-        MediaStream(
-            url,
-            stream_type=MediaStream.Both
-        )
+        VideoPiped(url)
     )
 
 async def stop_stream(call_py, chat_id):
     await call_py.leave_group_call(chat_id)
-    
+        
