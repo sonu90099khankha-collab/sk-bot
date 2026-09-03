a1f1bc74ffebe7a29eca5c656d97f2fde8ff9abc
sonu90099khankha-collab <sonu90099khankha@gmail.com>
2026-09-03 15:47:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -161,9 +161,13 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        loop = asyncio.get_event_loop()
+        loop = asyncio.get_event_loop_policy().get_event_loop()
     except RuntimeError:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
     
-    loop.run_until_complete(main())
+    if loop.is_running():
+        asyncio.ensure_future(main())
+    else:
+        loop.run_until_complete(main())
+        
